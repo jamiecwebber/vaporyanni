0d044ef48b65c35892e9e704a9cdec6a215e4fcf
Jamie Christopher Webber <jamiecwebber@gmail.com>
2019-05-31 20:47:51 -0400

    changed sorting parameters

--- dash_project/datascraper.py
+++
@@ -1,9 +0,0 @@
-import pandas_datareader.data as web
-import datetime
-
-start = datetime.datetime(2017,1,1)
-end = datetime.datetime(2019,4,25)
-
-df = web.DataReader('TSLA', 'yahoo', start, end)
-
-print(df.head())
